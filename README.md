25a9b4fca784ac61b36682f5e29471af696a25cc
Jonathan Calvin <jmatc1@gmail.com>
2014-10-24 20:23:28 -0500

    reworking markup for new design

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Shape Docs
+[![Code Climate](https://codeclimate.com/github/jmatc/shape-docs/badges/gpa.svg)](https://codeclimate.com/github/jmatc/shape-docs)
+---
 Get what you need:
 ---
 ```
--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,12 +1,12 @@
-module.exports = {
-  options: {
-    beautify: true,
-    mangle: false,
-    compress: false
-  },
-  "default": {
-    files: {
-      '_public/assets/javascripts/vendor/prismjs/prism.js': ['bower_components/prismjs/prism.js']
-    }
-  }
-};
+// module.exports = {
+//   options: {
+//     beautify: true,
+//     mangle: false,
+//     compress: false
+//   },
+//   "default": {
+//     files: {
+//       '_public/assets/javascripts/vendor/prismjs/prism.js': ['bower_components/prismjs/prism.js']
+//     }
+//   }
+// };
